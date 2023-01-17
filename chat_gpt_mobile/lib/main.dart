3032d5319c5c0346a0b5e01166f455cbed1e4f96
Sev-people <07marczoyd.murray@gmail.com>
2023-01-17 16:07:09 +0100

    changes

--- chat_gpt_mobile/lib/main.dart
+++ chat_gpt_mobile/lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+ 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
-
+ 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+ 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -18,33 +18,55 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
         ),
-        home: MyHomePage(),
+        home: const MyHomePage(),
       ),
     );
   }
 }
-
+ 
 class MyAppState extends ChangeNotifier {
-
+  final user1 = 'Sev';
+  final user2 = 'Marc';
+ 
+  var user = '';
+ 
+  void signIn(name) {
+    switch(name) {
+      case 'Sev':
+      user = user1;
+      break;
+      case 'Marc':
+      user = user2;
+      break;
+      default:
+      user = '';
+      break;
+    }
+  }
 }
-
+ 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
+ 
 class _MyHomePageState extends State<MyHomePage> {
   var selectedIndex = 0;
-
+ 
   @override
   Widget build(BuildContext context) {
     Widget page;
       switch (selectedIndex) {
         case 0:
-          page = GeneratorPage();
+          page = ProfilePage();
           break;
         case 1:
-          page = Placeholder();
+          page = GeneratorPage();
+          break;
+        case 2:
+          page = const Placeholder();
           break;
         default:
           throw UnimplementedError('no widget for $selectedIndex');
@@ -57,7 +79,11 @@ class _MyHomePageState extends State<MyHomePage> {
               SafeArea(
                 child: NavigationRail(
                   extended: constraints.maxWidth >= 600,
-                  destinations: [
+                  destinations: const [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.person),
+                      label: Text('My account'),
+                    ),
                     NavigationRailDestination(
                       icon: Icon(Icons.home),
                       label: Text('Home'),
@@ -88,34 +114,95 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
+ 
 class GeneratorPage extends StatelessWidget {
+  final TextEditingController _controller = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final _focusNode = FocusNode();
+
+  GeneratorPage({super.key});
+ 
   @override
   Widget build(BuildContext context) {
     return Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            SingleChildScrollView(
-              reverse: true,
-              child: Container(
-                margin: EdgeInsets.all(30),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: TextField(
-                minLines: 1,
-                maxLines: 8,
-                textInputAction: TextInputAction.go,
-                decoration: InputDecoration(
-                hintText: "Ask me a question",
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
+                    key: _formKey,
+                    reverse: true,
+                    child: Container(
+                      margin: const EdgeInsets.all(30),
+                      alignment: Alignment.center,
+                      child: TextFormField(
+                        focusNode: _focusNode,
+                        onFieldSubmitted: (value) {
+                        _formKey.currentState?.validate();
+                        _formKey.currentState?.save();
+                        _controller.clear();
+                        },
+                        controller: _controller,
+                        minLines: 1,
+                        maxLines: 8,
+                        decoration: const InputDecoration(
+                        hintText: "Ask me a question",
+                        ),
+                        textInputAction: TextInputAction.go
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-                ),
+                Container(
+                  margin: const EdgeInsets.only(right: 30),
+                  child: ElevatedButton(
+                    onPressed: () => {
+                      TextInputAction.go,
+                      _controller.clear(),
+                    },
+                    child: const Icon(Icons.send),
+                  ),
+                )
+              ],
             ),
           ],
         ),
-      );
+    );
+  }
+}
+ 
+class ProfilePage extends StatelessWidget {
+  final TextEditingController _controller = TextEditingController();
+
+  ProfilePage({super.key});
+ 
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: const EdgeInsets.all(30),
+            child: TextField(
+              controller: _controller,
+              onSubmitted: (value) => {
+                appState.signIn(value),
+                // ignore: avoid_print
+                print(appState.user),
+                _controller.clear(),
+              },
+              decoration: const InputDecoration(
+                hintText: 'Sign in with Google',
+              ),
+            ),
+          ),
+        ]
+      )
+    );
   }
 }
